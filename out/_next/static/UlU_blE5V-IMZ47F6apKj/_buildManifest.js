0b30eee5c8c48a044977b45c5037b2d376372480
Mike Littman <contact@mikelittman.me>
2022-04-10 11:16:53 -0500

    Deploy Next.js to gh-pages

---
+++ out/_next/static/UlU_blE5V-IMZ47F6apKj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/60016b508fdbf670.css","static/chunks/pages/index-47a066dbc0d1033a.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
