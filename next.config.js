716628d7140658632ad71edeabad98781f58ad05
Mike Littman <contact@mikelittman.me>
2023-12-20 23:49:01 -0500

    update deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
